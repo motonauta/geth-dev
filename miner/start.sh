c9f7f7c346152e12ed16402d1466994fe5f521c8
Nacho Althabe <nacho.althabe@gmail.com>
2020-10-05 10:21:34 -0300

    Fix

--- miner/start.sh
+++ miner/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 geth init genesis.json
-geth --bootnodes "enode://$bootnodeId@$bootnodeIp:30305" --networkid "6660001" --syncmode=full --mine --gasprice "0" --etherbase $address --unlock $address --password ~/.accountpassword --nousb --nat "none"
+geth --bootnodes "enode://$bootnodeId@$bootnodeIp:30305" --networkid "6660001" --syncmode=full --mine --gasprice "1" --etherbase $address --unlock $address --password ~/.accountpassword --nousb --nat "none"
--- node/start.sh
+++ node/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 geth init genesis.json
-geth --bootnodes "enode://$bootnodeId@$bootnodeIp:30305" --networkid="6660001" --rpc --rpcaddr="0.0.0.0" --rpcapi="eth,web3,net,admin,debug" --rpccorsdomain="*" --rpcvhosts="*" --syncmode="full" --nousb --graphql --graphql.addr="0.0.0.0" --graphql.corsdomain="*" --graphql.vhosts="*" --ws --wsaddr="0.0.0.0" --wsapi="eth,web3,net" --wsorigins="*"  --nat "none"
+geth --syncmode "full" --gcmode "archive" --bootnodes "enode://$bootnodeId@$bootnodeIp:30305" --networkid="6660001" --rpc --rpcaddr="0.0.0.0" --rpcapi="eth,web3,net,admin,debug,txpool" --rpccorsdomain="*" --rpcvhosts="*" --syncmode="full" --nousb --graphql --graphql.corsdomain="*" --graphql.vhosts="*" --ws --wsaddr="0.0.0.0" --wsapi="eth,web3,net" --wsorigins="*"  --nat "none"
